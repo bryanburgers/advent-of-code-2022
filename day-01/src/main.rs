2056b65b6bff9d789e1c12a3f329e2e55ee165e3
Bryan Burgers <bryan@burgers.io>
2022-11-30 23:15:48 -0600

    01b

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::BTreeSet;
+
 fn main() {
     let input = include_str!("input.txt");
     println!("{}", part_1(input));
+    println!("{}", part_2(input));
 }
 
 fn part_1(str: &str) -> u64 {
@@ -22,6 +25,28 @@ fn part_1(str: &str) -> u64 {
     max
 }
 
+fn part_2(str: &str) -> u64 {
+    let mut set = BTreeSet::new();
+
+    let mut current = 0;
+    for line in str.lines() {
+        if line.is_empty() {
+            set.insert(current);
+            current = 0;
+            continue;
+        }
+
+        let val = line.parse::<u64>().unwrap();
+        current += val;
+    }
+
+    if current > 0 {
+        set.insert(current);
+    }
+
+    set.into_iter().rev().take(3).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +56,12 @@ mod tests {
         let input = include_str!("example.txt");
         let result = part_1(input);
         assert_eq!(result, 24_000);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = include_str!("example.txt");
+        let result = part_2(input);
+        assert_eq!(result, 45_000);
     }
 }
