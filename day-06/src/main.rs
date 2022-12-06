882fa34ca7632e0b520b0ad35f738f693840f1d0
Bryan Burgers <bryan@burgers.io>
2022-12-06 07:01:20 -0600

    06b

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let input = include_str!("input.txt").trim();
     let result = part_1(input);
     println!("{result}");
+    let result = part_2(input);
+    println!("{result}");
 }
 
 fn part_1(str: &str) -> usize {
@@ -28,6 +30,32 @@ fn is_valid_quad(str: &str) -> bool {
         && bytes[2] != bytes[3]
 }
 
+fn part_2(str: &str) -> usize {
+    let len = str.len();
+    for end in 14..len {
+        let start = end - 14;
+        let substr = &str[start..end];
+        if is_valid_start_of_message(substr) {
+            return end;
+        }
+    }
+    panic!()
+}
+
+#[inline(always)]
+fn is_valid_start_of_message(str: &str) -> bool {
+    // Assumes the string is 14 characters long!
+    let bytes = str.as_bytes();
+    for i in 0..14 {
+        for j in (i + 1)..14 {
+            if bytes[i] == bytes[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +67,14 @@ mod tests {
         assert_eq!(part_1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
         assert_eq!(part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
         assert_eq!(part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(part_2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(part_2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
     }
 }
